ce761a521e71156a4969add0eb70d72d7a42b454
Jaime <Jaime.FernandezBravo1@alu.uclm.es>
2017-02-27 11:16:43 +0100

    btnAbrirArchivo
    funciona el boton abrir archivo y muestra la ruta del archivo abierto

--- Practica2BBDD/Form1.Designer.vb
+++ Practica2BBDD/Form1.Designer.vb
@@ -22,9 +22,40 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.BtnAbrirBBDD = New System.Windows.Forms.Button()
+        Me.lblRuta = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BtnAbrirBBDD
+        '
+        Me.BtnAbrirBBDD.Location = New System.Drawing.Point(23, 250)
+        Me.BtnAbrirBBDD.Name = "BtnAbrirBBDD"
+        Me.BtnAbrirBBDD.Size = New System.Drawing.Size(75, 23)
+        Me.BtnAbrirBBDD.TabIndex = 0
+        Me.BtnAbrirBBDD.Text = "Abrir BBDD"
+        Me.BtnAbrirBBDD.UseVisualStyleBackColor = True
+        '
+        'lblRuta
+        '
+        Me.lblRuta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblRuta.Location = New System.Drawing.Point(104, 250)
+        Me.lblRuta.Name = "lblRuta"
+        Me.lblRuta.Size = New System.Drawing.Size(280, 69)
+        Me.lblRuta.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(408, 396)
+        Me.Controls.Add(Me.lblRuta)
+        Me.Controls.Add(Me.BtnAbrirBBDD)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents BtnAbrirBBDD As Button
+    Friend WithEvents lblRuta As Label
 End Class
--- Practica2BBDD/Form1.vb
+++ Practica2BBDD/Form1.vb
@@ -1,3 +1,15 @@
 ﻿Public Class Form1
 
+    Private Sub BtnAbrirBBDD_Click(sender As Object, e As EventArgs) Handles BtnAbrirBBDD.Click
+
+        Dim ventanaAbrirBBDD As New OpenFileDialog()
+        ventanaAbrirBBDD.Title = "Selecciona una base de datos"
+        ventanaAbrirBBDD.Filter = "Archivos de base de datos|*.accbd"
+
+        If ventanaAbrirBBDD.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Me.Cursor = New Cursor(ventanaAbrirBBDD.OpenFile())
+            lblRuta.Text = ventanaAbrirBBDD.FileName
+        End If
+
+    End Sub
 End Class
